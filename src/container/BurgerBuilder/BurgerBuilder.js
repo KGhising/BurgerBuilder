fcd2c60052fc74e3afe1283dcdcfa8510f9229a6
Kishor Ghising <bhfty99@gmail.com>
2020-04-26 13:44:12 +0545

    Adding ingredients in burger dynamic

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
 import Auxilary from '../../hoc/Auxilary';
+import Burger from '../../component/Burger/Burger';
 
 class BurgerBuidler extends Component{
+    // you can also add constructor     
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
     render() {
         return (
             <Auxilary>
-                <div>
-                    Burger
-                </div>
-                <div>
-                    Build Control
-                </div>
+                <Burger ingredients={this.state.ingredients} />
             </Auxilary>
         );
     }

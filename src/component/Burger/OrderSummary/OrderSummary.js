36bbd8b3f0dd8a86b00884aa82fcc6a3ddc7f434
Kishor Ghising <bhfty99@gmail.com>
2020-04-28 16:41:14 +0545

    Implement react lifecycle to improve unnecessary Modal render

--- src/component/Burger/OrderSummary/OrderSummary.js
+++ src/component/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxilary from '../../../hoc/Auxilary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component{
+    componentDidUpdate(){
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
         return <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>
         });
     return (
@@ -17,12 +22,13 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-    <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to CheckOut?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
         </Auxilary>
     );
-};
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;

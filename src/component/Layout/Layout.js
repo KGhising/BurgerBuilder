fe55eec6570e257585940535bbcc09988132dde1
Kishor Ghising <bhfty99@gmail.com>
2020-04-28 16:10:52 +0545

    Implement full functional sidebar

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxilary from '../../hoc/Auxilary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const layout = (props) => (
-    <Auxilary>
-        <Toolbar />         
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxilary>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerhandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    sideDrawerTogglehandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render () {
+        return (
+            <Auxilary>
+                <Toolbar toggleClicked={this.sideDrawerTogglehandler} />
+                <Sidedrawer open={this.state.showSideDrawer} closed={this.sideDrawerhandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxilary>
+        )
+    }
+}
+
+export default Layout;
--- src/component/Navigation/Toolbar/Toolbar.js
+++ src/component/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,15 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Toggle from '../Sidedrawer/Toggle/Toggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
+        <Toggle clicked={props.toggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
